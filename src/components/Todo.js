2c5426aafd2db55c1ffd3e723d1cfa0de3337fb3
Gusti Oka <gusti.oka@iproperty.com>
2017-04-12 18:13:09 +0700

    Completing multi language handler via redux store applyMiddleware()

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react"
+import React from "react"
+import PropTypes from 'prop-types'
 
 class Todo extends React.Component
 {
@@ -16,7 +17,7 @@ class Todo extends React.Component
     {
         return (
             <li
-              onClick={() => this.handlerClick()} 
+              onClick={() => this.handlerClick()}
               style={{textDecoration: this.props.completed ? "line-through" : "none"}}
             >
                 {this.props.text}
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import { createPolyglotMiddleware } from 'redux-polyglot'
 import todoApp from "./reducers/reducers"
+import { setLocale } from './actions/actionCreators'
 
 const languages = {
   "en": {
@@ -25,15 +26,45 @@ const languages = {
   }
 }
 
-const polyglotMiddleware = createPolyglotMiddleware(
+/*const actionsShouldBeHandled = [
+  '@@router/LOCATION_CHANGEOOO'
+];
+
+const locationChangeMiddleware = () => {
+  return store => next => action => {
+    next(action)
+    const {type} = {action}
+
+    if (actionsShouldBeHandled.includes(type)) {
+      console.log('MASUK TYPE @@router/LOCATION_CHANGE')
+
+      const {
+        payload: {
+          pathname,
+          action: routeAction,
+          query
+        }
+      } = action;
+
+      if (routeAction === 'POP') {
+        store.dispatch(setLocale("en"));
+      }
+    }
+  }
+}*/
+
+const middlewares = [createPolyglotMiddleware(
   ['SET_LOCALE'],
   action => action.payload.locale,
   locale => new Promise(resolve => {
     resolve(languages[locale])
   })
-)
+)]
 
-const store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(polyglotMiddleware))
+const appliedMiddleware = applyMiddleware(...middlewares);
+// const createReduxStore = compose(appliedMiddleware, window.devToolsExtension())
+const createReduxStore = appliedMiddleware
+const store = createReduxStore(createStore)(todoApp, window.__INITIAL_STATE__);
 
 if (module.hot) {
     module.hot.accept('./reducers/reducers', () => {
